26cfc2cf8d96aabd78b3d44cf40124746455a4f3
gonzalde <30518927+gonzalde@users.noreply.github.com>
2019-01-28 15:26:00 -0600

    Move out navbar into single component

--- nashville-events/src/App.jsx
+++ nashville-events/src/App.jsx
@@ -1,33 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavItem, Nav } from 'react-bootstrap';
-import EventList from './EventList.jsx';
+import EventList from './EventList';
+import NashNav from './NashNav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <div className="text-logo">
-                {/* <img src={logo} width="30" height="30" alt="Logo"></img> */}
-                <b>NASH</b>
-              </div>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              Events
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Housing
-            </NavItem>
-            <NavItem eventKey={3} href="#">
-              Budget
-            </NavItem>
-          </Nav>
-        </Navbar>
+        <NashNav />
         <div className="container main-content">
           <h1>Events</h1>
           <EventList />
