0bece48972e4ef6955584d37906637ca55aa12cd
gonzalde <30518927+gonzalde@users.noreply.github.com>
2019-02-04 16:13:10 -0600

    Move budget into single project

---
+++ nashville-events/src/events-page/event-fetcher/nashvilleEvents.js
@@ -0,0 +1,28 @@
+import firebase from '../firebase.js';
+
+const request = require('request');
+const cheerio = require('cheerio');
+
+const events = firebase.database().ref('nashville_events');
+
+const url = 'https://www.visitmusiccity.com/visitors/events/upcomingevents';
+
+request(url, (error, response, body) => {
+  if (!error) {
+    const $ = cheerio.load(body);
+    const title = $('title').text();
+    const events = [];
+    $('.events')
+      .find('li')
+      .each(function () {
+        let current = $(this).text();
+        current = current.replace('@', '-');
+        events.push();
+        console.log(current);
+      });
+    console.log(`URL: ${url}`);
+    console.log(`Title: ${title}`);
+  } else {
+    console.log(`We’ve encountered an error: ${error}`);
+  }
+});
