179df3f6d74c962b30b2fd47c70777b98843633b
hannahgracej <hannahgracej2017@gmail.com>
2019-02-11 16:15:42 -0600

    added to EventCard

---
+++ nashville-events/src/events-page/EventCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap';
+
+export default function EventCard() {
+  return (
+    <div class="card">
+  <div class="card-header">Header</div>
+  <div class="card-body">Content</div> 
+  <div class="card-footer">Footer</div>
+</div>
+  );
+}
