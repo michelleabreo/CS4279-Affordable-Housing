976685aefc9cf41167ae5c24c570d8c60fbeae4d
hannahgracej <hannahgracej2017@gmail.com>
2019-02-18 09:34:47 -0600

    got card working?

--- nashville-events/src/events-page/EventCard.jsx
+++ nashville-events/src/events-page/EventCard.jsx
@@ -1,28 +1,51 @@
 import React from 'react';
 import 'bootstrap';
 
+
 export default function EventCard() {
   return (
-    <div class="card">
-    // style="width: 18rem;"
-    {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
-    { <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-      <li class="list-group-item">Vestibulum at eros</li>
-    </ul>
-    <div class="card-body">
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
-    </div> }
+<div class="card" style={{width : '18rem'}}>
+    {/* <div class="card" style={{float : 'left', paddingRight : '5px'}}> */}
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   
+  </div>
+</div>
 
-   <div class="card-header">Header</div>
-   <div class="card-body">Content</div> 
-   <div class="card-footer">Footer</div>
- </div>
+    //   <Card style={{ width: '18rem' }}>
+  // <Card.Body>
+  //   <Card.Title>Card Title</Card.Title>
+  //   <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+  //   <Card.Text>
+  //     Some quick example text to build on the card title and make up the bulk of
+  //     the card's content.
+  //   </Card.Text>
+  //   <Card.Link href="#">Card Link</Card.Link>
+  //   <Card.Link href="#">Another Link</Card.Link>
+  // </Card.Body>
+  // </Card>);
+    
+//     <div class="card">
+//     // style="width: 18rem;"
+//     {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
+//     { <div class="card-body">
+//       <h5 class="card-title">Card title</h5>
+//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     </div>
+//     <ul class="list-group list-group-flush">
+//       <li class="list-group-item">Cras justo odio</li>
+//       <li class="list-group-item">Dapibus ac facilisis in</li>
+//       <li class="list-group-item">Vestibulum at eros</li>
+//     </ul>
+//     <div class="card-body">
+//       <a href="#" class="card-link">Card link</a>
+//       <a href="#" class="card-link">Another link</a>
+//     </div> }
+
+//    <div class="card-header">Header</div>
+//    <div class="card-body">Content</div> 
+//    <div class="card-footer">Footer</div>
+//  </div>
   );
 }
