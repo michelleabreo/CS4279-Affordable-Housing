420c099ef3872e975121fd9710117be50b29ec7c
gonzalde <30518927+gonzalde@users.noreply.github.com>
2019-02-26 12:16:34 -0600

    Update to have marker move around

--- nashville-events/src/JSFiles/GMaps.js
+++ nashville-events/src/JSFiles/GMaps.js
@@ -13,12 +13,13 @@ function buildInfoCard(nHoodData) {
   const infowindow = new google.maps.InfoWindow({
     content: contentString,
   });
-  console.log(infowindow);
   return infowindow;
 }
 
 function initMap() {
   const trafficLayer = new google.maps.TrafficLayer();
+  const bikeLayer = new google.maps.BicyclingLayer();
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 36.1627, lng: -86.7816 },
     zoom: 14,
@@ -31,14 +32,17 @@ function initMap() {
     'https://raw.githubusercontent.com/michelleabreo/CS4279-Affordable-Housing/master/Maps/Updated_Zillow.geojson',
   );
   let infoWindow;
+  let marker;
   map.data.addListener('mouseover', (event) => {
     map.data.revertStyle();
     map.data.overrideStyle(event.feature, { fillColor: 'blue' });
     infoWindow = buildInfoCard(event.feature.l);
-    const marker = new google.maps.Marker({
+    const lat = event.latLng.lat();
+    const lng = event.latLng.lng();
+    marker = new google.maps.Marker({
       position: {
-        lat: 36.1627,
-        lng: -86.7816,
+        lat,
+        lng,
       },
       map,
       title: 'RegionID',
@@ -48,6 +52,8 @@ function initMap() {
   map.data.addListener('mouseout', () => {
     map.data.revertStyle();
     infoWindow.close();
+    marker.setMap(null);
   });
+  bikeLayer.setMap(map);
   trafficLayer.setMap(map);
 }
