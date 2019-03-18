445015d44a5cb93bd3077fc29c3f6bcefc396909
evansegaul <evan.segaul@vanderbilt.edu>
2019-03-18 16:29:38 -0500

    Adding input fields to budget page

--- nashville-events/src/components/BudgetPage2.js
+++ nashville-events/src/components/BudgetPage2.js
@@ -4,22 +4,23 @@ import '../styling/BudgetPage.css';
 {/**The structure of this page will be as follows:
 
  Budget
-    SalaryForm
-    ExpenditureTable
-        ExpenditureRow
-            ExpenditureName
-            ExpenditureValue
-            ExpenditureChange   */}
+ SalaryForm
+ ExpenditureTable
+ ExpenditureRow
+ ExpenditureName
+ ExpenditureValue
+ ExpenditureChange   */
+}
 
 class BudgetPage2 extends Component {
     render() {
-        return(
+        return (
             <div className={"Budget Page"}>
                 <header>
                     <h1>Budget Page 2: The better version</h1>
                 </header>
                 <body>
-                    <Budget />
+                <Budget/>
                 </body>
             </div>
         );
@@ -30,7 +31,7 @@ class Budget extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: '',
+            salary: '',
             percentages: {
                 housing: 0.275,
                 insurance: 0.1,
@@ -43,61 +44,119 @@ class Budget extends Component {
                 personal: 0.075,
             },
         };
+
+        this.handleSalaryChange = this.handleSalaryChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.handleFoodChange = this.handleFoodChange.bind(this);
     }
 
     handleFoodChange(event) {
+        console.log("handleFoodChange called.");
         let tmpPercents = this.state.percentages;
         tmpPercents.food = event.target.value;
+        this.setState({percentages: tmpPercents});
+    }
+
+    handleSalaryChange(event) {
+        this.setState({salary: event.target.value});
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault();
+        console.log('handleFormSubmit called');
+        const tmpPercents = this.state.percentages;
+
+
+        tmpPercents.housing = parseFloat(this.housingPercent.value);
+        tmpPercents.insurance = parseFloat(this.insurancePercent.value);
+        tmpPercents.food = parseFloat(this.foodPercent.value);
+
+        {/*
+        tmpPercents.transportation = parseFloat(this.transportationPercent.value);
+        tmpPercents.utilities = parseFloat(this.utilitiesPercent.value);
+        tmpPercents.savings = parseFloat(this.savingsPercent.value);
+        tmpPercents.fun = parseFloat(this.funPercent.value);
+        tmpPercents.clothing = parseFloat(this.clothingPercent.value);
+        tmpPercents.personal = parseFloat(this.personalPercent.value);
+        */
+        }
+
+
         this.setState({percentages: tmpPercents});
 
     }
 
     render() {
         const ExpenditureChange = (parameters) => {
-            return(
+            return (
                 <td>
-                    <div>
-                        <input type={"number"}
-                               step={.001}
-                               defaultValue={parameters.perc}
-                               name={"food"}
-                               onSubmit={parameters.func} />
-                    </div>
-                    <div>
-                        Button to change percent
-                    </div>
+                    <input type={"number"}
+                           step={.001}
+                           defaultValue={parameters.perc}
+                           name={"food"}
+                           ref={(input) => this.foodPercent = input}
+                    />
                 </td>
             );
         };
 
         const ExpenditureRow = (parameters) => {
-            return(
+            return (
                 <tr>
-                    <td>{parameters.category}</td>
+                    <td>{parameters.category} ({parameters.percent * 100}%):</td>
                     <td>{parameters.percent * this.state.salary}</td>
-                    <td><ExpenditureChange perc={parameters.percent} func={parameters.func}/></td>
+                    <td>
+                        <ExpenditureChange
+                            perc={parameters.percent}
+                            varToChange={parameters.ref}
+                        />
+                    </td>
                 </tr>
             );
         };
 
         const ExpenditureTable = () => {
-            return(
-                <table>
-                    <th>Expenditures</th>
-                    <tr>
+            return (
+                <div>
+                    <form onSubmit={this.handleFormSubmit}>
+                        <ExpenditureRow
+                            category={"Housing"}
+                            percent={this.state.percentages.housing}
+                            ref={this.housingPercent}
+                        />
+                        <ExpenditureRow
+                            category={"Insurance"}
+                            percent={this.state.percentages.insurance}
+                            ref={this.insurancePercent}
+                        />
                         <ExpenditureRow
                             category={"Food"}
                             percent={this.state.percentages.food}
-                            func={this.handleFoodChange}/>
-                    </tr>
-                </table>
+                            ref={this.foodPercent}
+                        />
+                        <ExpenditureRow
+                            category/>
+                    </form>
+                </div>
             );
         };
 
-        return(
-            <ExpenditureTable />
+        return (
+            <table>
+                <th>Expenditures</th>
+                <tr>
+                    Monthly salary:
+                    <input type="number" value={this.state.salary} autoFocus={true}
+                           onChange={this.handleSalaryChange}/>
+                </tr>
+                <tr>
+                    <ExpenditureTable/>
+                </tr>
+            </table>
         );
 
     }
 }
 
+
+export default BudgetPage2;
