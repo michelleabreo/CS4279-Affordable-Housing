df0f2296a76187a03a08f0bf131628bbee14a18e
gonzalde <30518927+gonzalde@users.noreply.github.com>
2019-03-28 12:42:09 -0500

    Add calls to Yelp in express server.

--- nashville-events/server/index.js
+++ nashville-events/server/index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
+const routes = require('./routes/routes.js');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
-);
+routes(app);
+
+app.listen(3001, () => console.log('Express server is running on localhost:3001'));
---
+++ nashville-events/server/routes/routes.js
@@ -0,0 +1,51 @@
+const yelp = require('yelp-fusion');
+
+const client = yelp.client(
+  'nJvRCUJ94q0VVptIYGXzrzauyTaITN9k2mHFyquWt_8-t_zS6Mdit2TmDw_Wtaw-5dYTUjHzw2v0uihDjEt8x8a_1LBv34IDp8RgVztNzVBNOrmyQNT42z8amWSIXHYx',
+);
+
+const appRouter = function (app) {
+  app.get('/', (req, res) => {
+    res
+      .status(200)
+      .send({
+        message:
+          'Welcome to our yelp call API the allowed calls are get/groceries and get/childcare',
+      });
+  });
+
+  app.get('/groceries/:lat/:long', (req, res) => {
+    const lat = req.params.lat;
+    const long = req.params.long;
+    client
+      .search({
+        term: 'groceries',
+        latitude: lat,
+        longitude: long,
+      })
+      .then((response) => {
+        res.status(200).send(response.jsonBody);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+
+  app.get('/childcare/:lat/:long', (req, res) => {
+    const lat = req.params.lat;
+    const long = req.params.long;
+    client
+      .search({
+        term: 'childcare',
+        latitude: lat,
+        longitude: long,
+      })
+      .then((response) => {
+        res.status(200).send(response.jsonBody);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+};
+module.exports = appRouter;
