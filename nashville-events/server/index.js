f57c6f06e66b62ba7f593221ec42ac7a1702e09a
gonzalde <30518927+gonzalde@users.noreply.github.com>
2019-03-29 11:53:16 -0500

    Enable cors for express

--- nashville-events/server/index.js
+++ nashville-events/server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes/routes.js');
 
 const app = express();
-
+app.use(
+  cors({
+    origin: 'http://localhost:3001',
+  }),
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
